b9116c2e876bb6af74270f77b8dc020a078775b3
Mathieu DARTIGUES <mathieu@dartic.fr>
2017-06-14 17:01:26 +0200

    [step-1.1] Add html / css / js example files

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -1,1 +1,23 @@
-// write your code here !
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div className="HelloWorld">Hello {this.props.name}</div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HelloWorld name="World !" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
