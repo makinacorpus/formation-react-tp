3d47881029ef130ff51c53c8d641c0f4f209dc26
Mathieu DARTIGUES <mathieu@dartic.fr>
2017-06-15 23:05:20 +0200

    [step-6.2] Add react test renderer for snapshot testing

--- create-react-app/src/components/HelloWorld/index.test.js
+++ create-react-app/src/components/HelloWorld/index.test.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import renderer from 'react-test-renderer';
+
 import HelloWorld from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<HelloWorld />, div);
 });
+
+it('renders correctly to snapshot', () => {
+  const tree = renderer.create(
+    <HelloWorld />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+
+})
