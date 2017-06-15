691a8aae0860a9cdfcd45cf2a8cd8b77dc3f3d7a
Mathieu DARTIGUES <mathieu@dartic.fr>
2017-06-15 22:52:12 +0200

    [step-6.1] Add simple test for HelloWorld

--- create-react-app/src/components/HelloWorld/index.test.js
+++ create-react-app/src/components/HelloWorld/index.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HelloWorld from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<HelloWorld />, div);
 });
