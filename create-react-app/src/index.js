6dfc0a8e162e4a8b1b81970b507d87deb4913f11
Alexandra Janin <contact.ajanin@gmail.com>
2017-11-13 22:07:41 +0100

    [step-8.2] Add search route (App component)

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Paper from 'material-ui/Paper';
+import Menu from 'material-ui/Menu';
+import MenuItem from 'material-ui/MenuItem';
 import {
-  BrowserRouter as Router,
-  Route
+  Router,
+  Route,
+  Link
 } from 'react-router-dom'
 
+import createBrowserHistory from 'history/createBrowserHistory'
+
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store/';
 
 import './index.css';
+
+const customHistory = createBrowserHistory()
 
 const Home = () => (
   <div>
@@ -18,13 +27,36 @@ const Home = () => (
   </div>
 )
 
+const Routes = ({history}) => {
+  return (
+    <div>
+      <Paper className="Menu">
+        <Menu>
+          <MenuItem
+            disabled={history.location.pathname === '/home'}
+            containerElement={
+              <Link to="/home" />
+            }
+            primaryText="Home"/>
+            <MenuItem
+            disabled={history.location.pathname === '/search'}
+            containerElement={
+              <Link to="/search" />
+            }
+            primaryText="Map"/>
+        </Menu>
+      </Paper>
+      <Route path="/home" component={Home}/>
+      <Route path="/search" component={App}/>
+    </div>
+  )
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <Router>
-        <div>
-          <Route path="/home" component={Home}/>
-        </div>
+      <Router history={customHistory}>
+        <Routes history={customHistory} />
       </Router>
     </MuiThemeProvider>
   </Provider>
