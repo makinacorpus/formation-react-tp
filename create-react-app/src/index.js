6c99d09c73296b465e85aa000b642498c45b6a17
Alexandra Janin <contact.ajanin@gmail.com>
2017-11-13 22:04:10 +0100

    [step-8.1] Add Home route

--- create-react-app/src/index.js
+++ create-react-app/src/index.js
@@ -2,17 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store/';
 
 import './index.css';
 
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
 ReactDOM.render(
   <Provider store={store}>
     <MuiThemeProvider>
-      <App />
+      <Router>
+        <div>
+          <Route path="/home" component={Home}/>
+        </div>
+      </Router>
     </MuiThemeProvider>
   </Provider>
 , document.getElementById('root'));
